6ba51e7ea9e56e2e5c12819f8d76bd06c07cd1e1
Davi-html <davifernandes3674@gmail.com>
2024-08-08 20:14:00 -0300

    chore: add shacn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { Button } from "./_components/ui/button"
 function Home() {
   return (
     <>
-      <h1 className="text-red-500 mb-5 text-pretty underline bg-blue-500">Hello World</h1>
+      <Button>Hello</Button>
     </>
   );
 }
